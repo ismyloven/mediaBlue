620135e4fcba54cc0a9e095507ec267dd4874526
Nail Ismailov <nail.ismailov@binerals.com>
2022-12-26 16:38:24 +0200

    init project

--- README.md
+++ README.md
@@ -2,6 +2,12 @@ This is a [Next.js](https://nextjs.org/) project bootstrapped with [`create-next
 
 ## Getting Started
 
+First, copy .env.dist data to .env:
+
+```bash
+cp .env.dist .env
+```
+
 First, run the development server:
 
 ```bash
---
+++ src/pages/index.js
@@ -0,0 +1,41 @@
+import { useQuery, gql } from '@apollo/client'
+
+import styles from '../../styles/Home.module.scss'
+
+const QUERY = gql`
+  query Countries {
+    countries {
+      code
+      name
+      emoji
+    }
+  }
+`
+
+export default function Home() {
+  const { data, loading, error } = useQuery(QUERY)
+
+  if (loading) {
+    return <span>{'...Loading'} </span>
+  }
+
+  if (error) {
+    // console.error(error);
+    return null
+  }
+
+  const countries = data.countries.slice(0, 4)
+
+  return (
+    <div className={styles.body}>
+      {countries.map(country => (
+        <div key={country.code} className={styles.card}>
+          <h3>{country.name}</h3>
+          <p>
+            {country.code} - {country.emoji}
+          </p>
+        </div>
+      ))}
+    </div>
+  )
+}
